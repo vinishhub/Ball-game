e3d7d33962e57a9d211e53c626e891c825337ebb
VINISH <76252038+vinishhub@users.noreply.github.com>
2023-12-15 23:14:29 +0530

    first commit

---
+++ Assets/Scripts/playercontroller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playercontroller : MonoBehaviour
+{
+    public Text CountText;
+    public Text winText;
+    private int count; 
+    public float speed;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
+        rb.AddForce(movement * speed);
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+
+        }
+    }
+    void SetCountText() {
+        CountText.text = "Count:" + count.ToString();
+        if (count >= 11) {
+            winText.text = "You Win";
+        }
+
+    }
+    
+}
